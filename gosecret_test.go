45dc7fa0814fde3645c62eafe7d2bfb7b276b52f
Ryan Breen <rbreen@vistaprint.com>
2014-09-21 00:33:04 -0400

    Add key rotation test.

--- gosecret_test.go
+++ gosecret_test.go
@@ -2,7 +2,9 @@ package gosecret
 
 import (
 	"bytes"
+	"encoding/base64"
 	"io/ioutil"
+	"os"
 	"path"
 	"testing"
 )
@@ -49,6 +51,60 @@ func TestEncryptFile(t *testing.T) {
 
 	decrypted, err := DecryptTags(encrypted, "test_keys")
 
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Equal(plaintextFile, decrypted) {
+		t.Error("Encrypt / Decrypt round-trip failed")
+	}
+}
+
+func TestKeyRotation(t *testing.T) {
+
+	os.Remove(path.Join("test_keys", "test_key_1"))
+	os.Remove(path.Join("test_keys", "test_key_2"))
+
+	plaintextFile, err := ioutil.ReadFile(path.Join("test_data", "config_plaintext.json"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	file, err := ioutil.ReadFile(path.Join("test_data", "config.json"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a new key.  Use it to encrypt.
+	err = ioutil.WriteFile(path.Join("test_keys", "test_key_1"), []byte(base64.StdEncoding.EncodeToString(CreateKey())), 0666)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	encrypted, err := EncryptTags(file, "test_key_1", "test_keys", false)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create another new key.  Use it to re-encrypt.
+	err = ioutil.WriteFile(path.Join("test_keys", "test_key_2"), []byte(base64.StdEncoding.EncodeToString(CreateKey())), 0666)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	encrypted, err = EncryptTags(file, "test_key_2", "test_keys", true)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Delete the first key.
+	err = os.Remove(path.Join("test_keys", "test_key_1"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Decrypt the file
+	decrypted, err := DecryptTags(encrypted, "test_keys")
 	if err != nil {
 		t.Fatal(err)
 	}
